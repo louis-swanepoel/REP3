a87f1e5ecff65ae86da2875097361e8913ac70a9
lswanepoel <98822407+louis-swanepoel@users.noreply.github.com>
2022-03-31 15:32:43 +0100

    created functions for each step

--- code_with_energy_calculations.py
+++ code_with_energy_calculations.py
@@ -8,6 +8,7 @@ mass_of_jwt = 6000
 
 def ode(t, r):
     
+    
     G = 6.674*10-11
     m1 = 1.989*10**30
     m2 = 5.972*10**24
@@ -22,57 +23,72 @@ def ode(t, r):
     q2_dot = - ( (( 2 * r2 * q2 )/r1) + ( 2 * ( (math.cos(p1))/(math.sin(p1)) ) * p2 * q2 ) )
     
     return r1_dot, r2_dot, p1_dot, p2_dot, q1_dot, q2_dot
-time_start_orbit = 1000000
-time_end_orbit = 100000000
-r1i= 151101100000
-r2i= 320
+time_start_orbit = 0
+time_end_orbit = 1814400
+
+r1i= 151500000000
+r2i= 0
 p1i= (0.00039708/180)*math.pi
-p2i= 320/600000000
+p2i= 0.000000000009
 q1i=(0.00039708/180)*math.pi
-q2i= 320/600000000
+q2i= 0.00000009
 #sol = integrate.solve_ivp(ode,[0,1814400], [151101100,185],[(3.9708)*10^-3,185.32],[(3.9708*10^-6), 185], t_eval = np.linspace(0, 1814400, 10000))
-sol = integrate.solve_ivp(ode,[time_start_orbit,time_end_orbit], [r1i,r2i,p1i,p2i,q1i, q2i], t_eval = (time_start_orbit, time_end_orbit, 1000))
-
-
-
-x_values_list = []
-y_values_list = []
-z_values_list = []
-
-for i in range(0, len(sol.y[0])):
-    x_value = sol.y[0, i] * math.cos(sol.y[2, i]) * math.cos(sol.y[4, i])
-    x_values_list.append(x_value)
+  
+def integration(time_end_orbit,time_start_orbit,ode,r1i,r2i,p1i,p2i,q1i, q2i):
     
-    y_value = sol.y[0, i] * math.cos(sol.y[2, i]) * math.sin(sol.y[4, i])
-    y_values_list.append(y_value)
+    number_of_integrations = 10
     
-    z_value = sol.y[0, i] * math.sin(sol.y[2, i])
-    z_values_list.append(z_value)
+    sol = integrate.solve_ivp(ode,[time_start_orbit,time_end_orbit], [r1i,r2i,p1i,p2i,q1i, q2i], t_eval = np.linspace(time_start_orbit, time_end_orbit, number_of_integrations))    
+
+    return sol 
     
-print(z_values_list)
-print(y_values_list)
-print(z_values_list)
+def change_to_cartesian(sol):
+    
+    x_values_list = []
+    y_values_list = []
+    z_values_list = []
+    for i in range(0, len(sol.y[0])):
+        x_value = sol.y[0, i] * math.cos(sol.y[2, i]) * math.cos(sol.y[4, i])
+        x_values_list.append(x_value)
+        
+        y_value = sol.y[0, i] * math.cos(sol.y[2, i]) * math.sin(sol.y[4, i])
+        y_values_list.append(y_value)
+        
+        z_value = sol.y[0, i] * math.sin(sol.y[2, i])
+        z_values_list.append(z_value)
+
+time_in_a_day = 86400
+
+for time_end_orbit in range(0,time_end_orbit,time_in_a_day):
+    
+    integration(time_end_orbit,time_start_orbit,ode,r1i,r2i,p1i,p2i,q1i, q2i)
+    print(sol.y[0])
+
+     
+# print(y_values_list)
+# print(z_values_list)
 
 
 # method 2
 
-ax1 = plt.axes(projection = '3d')
-ax1.plot(x_values_list,
-        y_values_list,
-        z_values_list)
+# ax1 = plt.axes(projection = '3d')
+# ax1.plot( y_values_list, x_values_list,z_values_list)
+ 
+       
 
 # method 2
 
 # ax2 = plt.axes(projection = '3d')
-# ax2.plot(sol.y[0],
-#         sol.y[2],
+# ax2.plot(sol.y[2],
+#         sol.y[0],
 #         sol.y[4])
+
 # plt.show(ax2)
 
 # method for individual plots
 
-ax3 = plt.axes()
-ax3.plot((time_start_orbit, time_end_orbit, 100),sol.y[0])
+# ax3 = plt.axes()
+# ax3.plot(np.linspace(time_start_orbit, time_end_orbit, 1000),sol.y[0])
 
 # theta, phi = sol.y[2], sol.y[4]
 # THETA, PHI = np.meshgrid(theta, phi)
@@ -94,7 +110,7 @@ ax3.plot((time_start_orbit, time_end_orbit, 100),sol.y[0])
 
 # Scatter Graph
 
-# ax.scatter3D(x_values_list,
+# ax1.scatter3D(x_values_list,
 #         y_values_list,
 #         z_values_list)
 
